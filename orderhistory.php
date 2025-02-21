8aad63847c5976e029b3cde25b5b6abaed820de6
Furantuturu <fuwansuu@gmail.com>
2025-02-21 13:49:43 +0800

    handles getting all receipt id

--- orderhistory.php
+++ orderhistory.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Order History</title>
-</head>
-<body>
-    <h1>hello world</h1>
-</body>
-</html>
+<?php
+
+require_once 'Database.php';
+$db = new Database();
+
+$receipts = $db->query("SELECT DISTINCT receipt_id, total_price, created_at FROM coffee_order")->getAll();
+    
+require 'view.php';
+
+return view('orderhistory.view.php', [
+    'receipts' => $receipts,
+]);
