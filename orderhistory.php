26b5b74d19679ba4240607125a358e2028bbc00f
furantuturu <fuwansuu@gmail.com>
2025-03-03 22:19:55 +0800

    now the latest receipt show up first

--- orderhistory.php
+++ orderhistory.php
@@ -3,7 +3,7 @@
 require_once 'Database.php';
 $db = new Database();
 
-$receipts = $db->query("SELECT DISTINCT receipt_id, total_price, created_at FROM coffee_order")->getAll();
+$receipts = $db->query("SELECT DISTINCT receipt_id, total_price, created_at FROM coffee_order ORDER BY created_at DESC")->getAll();
     
 require 'view.php';
 
